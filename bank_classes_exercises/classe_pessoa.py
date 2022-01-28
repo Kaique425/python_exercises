4a9091d55984f06dd9c2e082244e5c5e9d9cd52c
Kaique Silva <kaique.silva@unimed197.local>
2022-01-28 17:34:38 -0300

    coding some classes as bank, account and customer.

--- bank_classes_exercises/classe_pessoa.py
+++ bank_classes_exercises/classe_pessoa.py
@@ -1,7 +1,11 @@
+import re
+from abc import ABC, abstractmethod
+
+
 class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
-        self.idade = idade
+        self._idade = idade
 
     @property
     def nome(self):
@@ -24,5 +28,120 @@ class Pessoa:
 
 pessoa = Pessoa("Kaique", 20)
 
-print(pessoa.nome)
-print(pessoa.idade)
+
+class Cliente(Pessoa):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+        self.conta = None
+
+    @property
+    def conta(self):
+        return self._conta
+
+    @conta.setter
+    def conta(self, conta):
+        self._conta = conta
+        return self._conta
+
+    def inserir_conta(self, conta):
+        self.conta = conta
+        return self.conta
+
+
+cliente = Cliente("Kaique", 20)
+"""print(cliente.nome)
+print(cliente.idade)
+print(cliente.conta)"""
+
+
+class Conta(ABC):
+    def __init__(self, agencia, numero_da_conta, saldo):
+        self.agencia = agencia
+        self.numero_da_conta = numero_da_conta
+        self._saldo = saldo
+
+    def saldo(self):
+        return self._saldo
+
+    def depositar(self, valor):
+        self.saldo += valor
+        return self.saldo
+
+    def detalhes(self):
+        print(
+            f"Agencia: {self.agencia} \n"
+            f"Numero da conta: {self.numero_da_conta} \n"
+            f"Saldo: {self.saldo}"
+        )
+        self.detalhes()
+
+    @abstractmethod
+    def sacar(self, valor):
+        pass
+
+
+class ContaPoupança(Conta):
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo = self.saldo - valor
+            print(
+                f"Você sacou {valor}, restando {self.saldo} como saldo atual da sua conta."
+            )
+            return self.saldo
+        else:
+            print(
+                f"Saldo insuficiente! você tentou sacar {valor} mas seu saldo é de {self.saldo} sendo assim faltou {re.sub(r'^-', '', str(self.saldo - valor))}"
+            )
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, numero_da_conta, saldo, limite):
+        super().__init__(agencia, numero_da_conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        limite = self.limite
+        limite_total = limite + self.saldo
+
+        saldo = limite_total - valor
+        if valor <= limite_total:
+            if saldo >= 0:
+                print(f"Você retirou {valor}, sendo R$ {saldo} seu saldo atual.")
+
+            elif saldo < 0:
+                print(f"você retirou {valor}, sua fatura será de {saldo}")
+
+            self.saldo = saldo
+            return self.saldo
+        else:
+            print(
+                f"você tentou retirar {valor} excedendo o limite atual da conta em {int(re.sub(r'^-','', str(saldo)))}!"
+            )
+
+
+class Banco:
+    def __init__(self):
+        self.clientes = []
+        self.contas = []
+        self.agencias = ["0001", "0002", "0003"]
+
+    def cadastrar_cliente(self, cliente):
+        self.clientes.append(cliente)
+
+    def inserir_conta(self, conta):
+        self.contas.append(conta)
+
+    def autenticar(self, cliente):
+        agencias = self.agencias
+        agencia_cliente = cliente.conta.agencia
+        print(cliente.conta.agencia)
+        if cliente in self.clientes and agencia_cliente in agencias:
+            print("Esse cliente não está autenticado")
+
+
+conta_poupança = ContaCorrente("0005", "908098890", 500, 500)
+cliente = Cliente("Kaique", 20)
+cliente.inserir_conta(conta_poupança)
+banco = Banco()
+banco.cadastrar_cliente(cliente)
+banco.autenticar(cliente)
