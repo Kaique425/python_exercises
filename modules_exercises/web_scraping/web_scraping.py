d187f725649287c3b6528a52b3dcab6f000b5824
Kaique Silva <kaique.silva@unimed197.local>
2022-02-07 13:32:28 -0300

    Using requests and beautfulsoup to extract site informations

--- modules_exercises/web_scraping/web_scraping.py
+++ modules_exercises/web_scraping/web_scraping.py
@@ -1,5 +1,19 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.youtube.com/?&ab_channel=pythonando"
+url = "https://stackoverflow.com/questions/tagged/python"
 response = requests.get(url)
+html = BeautifulSoup(response.text, "html.parser")
+
+for tag in html.select(".question-summary"):
+    question = tag.select_one(".question-hyperlink")
+    relative_time = tag.select_one(".relativetime")
+    votes = tag.select_one(".vote-count-post")
+    views = tag.select_one(".views")
+    cleaned_views = re.sub(r"[^0-9]", "", views.text)
+    print(question.text)
+    print(f"{cleaned_views} viram essa pergunta")
+    print(f"Essa pergunta tem {votes.text} votos")
+    print(relative_time.text)
